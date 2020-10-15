2d0d568575302951043bc294a9029222e5de4408
Christopher Spraggett <christopherspraggett@Mitech-Mac-Mini-One.home>
2020-10-15 09:36:30 -0400

    Style tables

--- src/javaScript.js
+++ src/javaScript.js
@@ -8,9 +8,9 @@ var message = new Vue({
         display: '',
         interval: 0
     },
-   mounted() {
+    mounted() {
         this.interval = setInterval(this.updateDate, 1000);
-   },
+    },
     beforeDestroy() {
         clearInterval(this.interval);
     },
@@ -26,153 +26,151 @@ var message = new Vue({
     }
 })
 
-    var department = new Vue({
+var department = new Vue({
     el: '#departments',
     data: {
-    depts: [],
-    failure: false
-},
+        depts: [],
+        failure: false
+    },
     mounted: function () {
-    this.getDepartments();
-},
+        this.getDepartments();
+    },
     methods: {
-    getDepartments: function() {
-    const request = `${window.Configs.host}/departments`
-    const requestOptions = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'omit',
-};
-    fetch(request, requestOptions)
-    .then(response => {
-    if (response.status === 200) {
-    console.log('hello')
-    return response.json();
-} else {
-    console.log('response: ', response.text())
-    throw response.text();
-}
+        getDepartments: function () {
+            const request = `${window.Configs.host}/departments`
+            const requestOptions = {
+                method: 'GET',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'omit',
+            };
+            fetch(request, requestOptions)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json();
+                    } else {
+                        console.log('response: ', response.text())
+                        throw response.text();
+                    }
+                })
+
+                .then(data => {
+                    return this.depts = data
+                })
+                .catch(error => {
+                    this.failure = true;
+                    if (error instanceof Promise) {
+                        error.then(errorMessage => {
+                            console.warn(errorMessage)
+                        });
+                    } else {
+                        console.warn(error);
+                    }
+                });
+
+        }
+    }
 })
 
-    .then(data => {
-    console.log('data: ', data.body)
-    return this.depts = data})
-    .catch(error => {
-    this.failure = true;
-    if (error instanceof Promise) {
-    error.then(errorMessage => {
-    console.warn(errorMessage)
-});
-} else {
-    console.warn (error);
-}
-});
-
-}
-}
-})
-
-    var employees = new Vue({
+var employees = new Vue({
     el: '#employees',
     data: {
-    employees: '',
-    gender: '',
-    hireDate: '',
-    failure: false
-},
+        employees: '',
+        gender: '',
+        hireDate: '',
+        failure: false
+    },
     methods: {
-    getEmployees: function(gender, hireDate) {
-    const request = `${window.Configs.host}/employees?gender=${this.gender}&hire_date=${this.hireDate}`
-    const requestOptions = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'omit',
-};
-    fetch(request, requestOptions)
-    .then(response => {
-    if (response.status === 200) {
-    return response.json();
-} else {
-    throw response.text();
-}
+        getEmployees: function (gender, hireDate) {
+            const request = `${window.Configs.host}/employees?gender=${this.gender}&hire_date=${this.hireDate}`
+            const requestOptions = {
+                method: 'GET',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'omit',
+            };
+            fetch(request, requestOptions)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json();
+                    } else {
+                        throw response.text();
+                    }
+                })
+                .then(data => {
+                    return this.employees = data
+                })
+                .catch(error => {
+                    this.failure = true;
+                    if (error instanceof Promise) {
+                        error.then(errorMessage => {
+                            console.warn(errorMessage)
+                        });
+                    } else {
+                        console.warn(error);
+                    }
+                })
+        }
+    }
 })
-    .then(data => {
-    return this.employees = data})
-    .catch(error => {
-    this.failure = true;
-    if (error instanceof Promise) {
-    error.then(errorMessage => {
-    console.warn(errorMessage)
-});
-} else {
-    console.warn (error);
-}
-})
-}
-}
-})
-    var createEmployee = new Vue({
+var createEmployee = new Vue({
     el: '#create-employee',
     data: {
-    empNo: '',
-    birthDate: '',
-    firstName: '',
-    lastName: '',
-    gender: '',
-    hireDate: '',
-    success: false,
-    failure: false,
-},
+        empNo: '',
+        birthDate: '',
+        firstName: '',
+        lastName: '',
+        gender: '',
+        hireDate: '',
+        success: false,
+        failure: false,
+    },
     methods: {
-    createEmployee: function() {
-    const request = new Request(`${window.Configs.host}/employees`);
-    const requestOptions = {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json'
-},
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'omit',
-    body: JSON.stringify({
-    emp_no: this.empNo,
-    birth_date: this.birthDate,
-    first_name: this.firstName,
-    last_name: this.lastName,
-    gender: this.gender,
-    hire_date: this.hireDate
-})
-};
-    fetch(request, requestOptions)
-    .then(response => {
-    console.log('status', response.status)
-    if (response.status === 200){
-    return response.text();
-}
-    else {
-    throw response.text();
-}
-})
-    .then(results => {
-    console.log(results);
-    this.success = true
-    this.failure = false
-})
-    .catch(error => {
-    this.success = false
-    this.failure = true
+        createEmployee: function () {
+            const request = new Request(`${window.Configs.host}/employees`);
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'omit',
+                body: JSON.stringify({
+                    emp_no: this.empNo,
+                    birth_date: this.birthDate,
+                    first_name: this.firstName,
+                    last_name: this.lastName,
+                    gender: this.gender,
+                    hire_date: this.hireDate
+                })
+            };
+            fetch(request, requestOptions)
+                .then(response => {
+                    console.log('status', response.status)
+                    if (response.status === 200) {
+                        return response.text();
+                    } else {
+                        throw response.text();
+                    }
+                })
+                .then(results => {
+                    console.log(results);
+                    this.success = true
+                    this.failure = false
+                })
+                .catch(error => {
+                    this.success = false
+                    this.failure = true
 
-    if (error instanceof Promise) {
-    error.then(errorMessage => {
-    console.warn(errorMessage);
-});
-}
-    else {
-    console.warn(error);
-}
-});
-}
-}
+                    if (error instanceof Promise) {
+                        error.then(errorMessage => {
+                            console.warn(errorMessage);
+                        });
+                    } else {
+                        console.warn(error);
+                    }
+                });
+        }
+    }
 })
